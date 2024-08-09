810f73bc7a2d44547faa20a79d09d3bb51d5fe8a
David Batista <139716873+dbat1sta@users.noreply.github.com>
2024-08-08 21:49:17 -0500

    Update memory.c

--- memory.c
+++ memory.c
@@ -1,40 +1,39 @@
 #include "memory.h"
 
-void memory_access(Instruction* inst, int* memory) {
+void memory_access(Instruction* inst, uint8_t* memory) {
     if (inst->memop == MEMOP_STORE) {
         switch (inst->load_type) {
             case STORE_BYTE:
-                memory[inst->result] = (uint8_t)inst->strval;
+                *((uint8_t*)(memory + inst->result)) = (uint8_t)inst->strval;
                 break;
             case STORE_HALF:
-                memory[inst->result] = (uint16_t)inst->strval;
+                *((uint16_t*)(memory + inst->result)) = (uint16_t)inst->strval;
                 break;
             case STORE_WORD:
-                memory[inst->result] = inst->strval;
+                *((uint32_t*)(memory + inst->result)) = inst->strval;
                 break;
             default:
                 break;
         }
     } else if (inst->memop == MEMOP_LOAD) {
-        int loaded_value = memory[inst->result];
         switch (inst->load_type) {
             case LOAD_BYTE:
-                loaded_value = (int8_t)loaded_value; // sign extends 8-bit 
+                inst->result = (int8_t)(*(int8_t*)(memory + inst->result)); 
                 break;
             case LOAD_HALF:
-                loaded_value = (int16_t)loaded_value; // sign extends 16-bit 
+                inst->result = (int16_t)(*(int16_t*)(memory + inst->result)); 
                 break;
             case LOAD_BYTE_U:
-                loaded_value = (uint8_t)loaded_value; // zero extends 8-bit 
+                inst->result = *(uint8_t*)(memory + inst->result); 
                 break;
             case LOAD_HALF_U:
-                loaded_value = (uint16_t)loaded_value; // zero extends 16-bit 
+                inst->result = *(uint16_t*)(memory + inst->result); 
                 break;
             case LOAD_WORD:
+                inst->result = *(uint32_t*)(memory + inst->result); 
                 break;
             default:
                 break;
         }
-        inst->result = loaded_value;
     }
 }
